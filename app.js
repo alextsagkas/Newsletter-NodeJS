ae7fdea7aff71e098e82533967ebc851aa7721c3
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-11-01 00:37:02 +0200

    initialize the project

---
+++ app.js
@@ -0,0 +1,60 @@
+import express from "express";
+import bodyParser from "body-parser";
+import client from "@mailchimp/mailchimp_marketing";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = 3000; // localhost port
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const apiKey = process.env.API_KEY;
+const server = apiKey.slice(apiKey.length - 4, apiKey.length);
+const listId = process.env.LIST_ID;
+
+client.setConfig({
+  apiKey: apiKey,
+  server: server,
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  // Add contact to audience
+  const data = {
+    email_address: email,
+    status: "subscribed",
+    merge_fields: {
+      FNAME: firstName,
+      LNAME: lastName,
+    },
+  };
+
+  async function run() {
+    try {
+      const response = await client.lists.addListMember(listId, data);
+      res.sendFile(`${process.cwd()}/success.html`);
+    } catch (error) {
+      res.sendFile(`${process.cwd()}/failure.html`);
+    }
+  }
+
+  run();
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(`${process.cwd()}/signup.html`);
+});
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`The app is running at port ${process.env.PORT || port}`);
+});
